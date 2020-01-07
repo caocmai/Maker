43d0146a3bd0d4094e7527ffec9b91335368d4d1
Cao Mai <caomai@rocketmail.com>
2020-01-07 11:01:00 -0800

    [add] success message to form

--- registrants/views.py
+++ registrants/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from registrants.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.messages.views import SuccessMessageMixin
 
 
-class SignUpView(CreateView):
+
+class SignUpView(SuccessMessageMixin, CreateView):
     form_class = UserCreationForm
     # success_url = reverse_lazy('registrationlogin')
     success_url = "test/"
     template_name = "registration/signup.html"
+    success_message = "An account was created successfully"
+
 
 # Create your views here.
 
