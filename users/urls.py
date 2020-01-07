5b181381f0a7c494e3665fbf7730b4684b77ab01
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-01-07 11:14:30 -0800

    testing3

--- users/urls.py
+++ users/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
-# from users.views import
-
-urlpatterns = [
-    path('', Home.as_view(), name='home-page'),
-
-
-]
+# from django.urls import path, include
+# # from users.views import
+#
+# urlpatterns = [
+#     path('', Home.as_view(), name='home-page'),
+#
+#
+# ]
