63a21e806e98e047c505facefa1a30f556bcf2dc
abrusebas1997 <sebastianabarca@Sebastians-MacBook-Pro.local>
2020-01-09 16:44:16 -0800

    finish chat

--- chat/consumers.py
+++ chat/consumers.py
@@ -8,16 +8,16 @@ from .models import Message
 User = get_user_model()
 
 class ChatConsumer(WebsocketConsumer):
-
     def fetch_messages(self, data):
         messages = Message.last_10_messages()
         content = {
+            'command': 'messages',
             'messages': self.messages_to_json(messages)
         }
         self.send_message(content)
 
     def new_message(self, data):
-        author = date['from']
+        author = data['from']
         author_user = User.objects.filter(username=author)[0]
         message = Message.objects.create(
             author=author_user,
@@ -31,34 +31,29 @@ class ChatConsumer(WebsocketConsumer):
     def messages_to_json(self, messages):
         result = []
         for message in messages:
-            result.append(self.messages_to_json(message))
+            result.append(self.message_to_json(message))
         return result
 
-    def messages_to_json(self, message):
+    def message_to_json(self, message):
         return {
             'author': message.author.username,
             'content': message.content,
             'timestamp': str(message.timestamp)
         }
 
-
-
     commands = {
         'fetch_messages': fetch_messages,
         'new_message': new_message
     }
 
-
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
         self.room_group_name = 'chat_%s' % self.room_name
-
         # Join room group
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
-
         self.accept()
 
     def disconnect(self, close_code):
@@ -83,9 +78,11 @@ class ChatConsumer(WebsocketConsumer):
                 'message': message
             }
         )
+    def send_message(self, message):
+        self.send(text_data=json.dumps(message))
 
     # Receive message from room group
     def chat_message(self, event):
         message = event['message']
         # Send message to WebSocket
-        (self.send(text_data=json.dumps(message)))
+        self.send(text_data=json.dumps(message))
--- chat/urls.py
+++ chat/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, re_path
-
-from . import views
+from .views import index, room
 
 app_name = 'chat'
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
+    path('', index, name='index'),
+    re_path(r'^(?P<room_name>[^/]+)/$', room, name='room'),
 ]
